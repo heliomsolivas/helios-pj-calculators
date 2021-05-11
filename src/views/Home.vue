18f5ffbcee8723ca25b09bc5d50fa154077ace31
Hélio M S Olivas <heliomsolivas@gmail.com>
2021-05-11 14:07:41 -0300

    Add disclaimer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,37 @@
 <template>
   <div class="home">
     <div class="container">
-      <img src="@/assets/helios-logo.png" alt="Helios Logo">
-      <h1>Helios Calculos tributários</h1>
-      <h2>Webapp criado com o intuito de ajudar novatos com cálculos tributários.</h2>
+      <h1 class="text-left pb-2">
+        Por que Programador (ou desenvolvedor) não podem ser MEI?
+      </h1>
+      <div
+        class="text-left pb-2"
+        style="margin-top:6px;font-weight:500;font-size:14px;"
+      >
+        Primeiramente precisamos destacar para quem realmente o MEI foi
+        criado.<br /><br />
+        O MEI foi criado para profissionais autônomos informais que têm
+        atividades que não são de cunho intelectual e científico.<br /><br />
+        Por exemplo as atividades de pedreiro, pipoqueiro, barbeiro
+        independente, diarista, dentre muitas outras pois a lista é grande.
+        <br /><br />
+
+        Portanto as atividades de cunho científico e intelectual não fazem parte
+        da lista de CNAEs (Classificação Nacional de Atividades Econômicas)
+        permitidas para o MEI.<br /><br />
+
+        E por isso que os CNAEs de Tecnologia da Informação não fazem parte das
+        atividades permitidas para o MEI.<br /><br />
+
+        E sendo assim você que é profissional de TI não pode ser MEI.
+      </div>
+      <h2 class="text-left pb-2">Disclaimer</h2>
+      <div
+        class="text-left pb-2"
+        style="margin-top:6px;font-weight:500;font-size:14px;"
+      >
+        Pense muito bem antes de optar pelo MEI, pois sua atividade como profissional de TI não permiti ser MEI.
+      </div>
     </div>
   </div>
 </template>
